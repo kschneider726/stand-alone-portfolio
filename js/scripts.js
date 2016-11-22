accbe450d6d41284c6b0977d1f55ee6e2c683f89
kschneider726 <kschneider726@users.noreply.github.com>
2016-11-22 14:35:12 -0800

    class upload 11/22/16

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 // My Scripts
+
+// Randomize Header
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor(Math.random() * maxRand);
+//console.log(numRand);
+
+// Store images
+var img0 = '/img/thumbs/2T.jpg';
+var img1 = '/img/thumbs/4T.jpg';
+var img2 = '/img/thumbs/6T.jpg';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
+
+// Activate Gallery
+$('figure').lightGallery({
+    selector: 'a'
+});
