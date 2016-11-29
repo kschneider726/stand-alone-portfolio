3e7df159a766365021451e9ca5c904d69db7db8d
kschneider726 <kschneider726@users.noreply.github.com>
2016-11-29 13:05:26 -0800

    make gallery work again

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-// My Scripts
-
 // Randomize Header
+
 // Store & Generate Number
 var maxRand = 3;
-var numRand = Math.floor(Math.random() * maxRand);
+var numRand = Math.floor( Math.random() * maxRand );
 //console.log(numRand);
 
 // Store images
-var img0 = '/img/thumbs/2T.jpg';
-var img1 = '/img/thumbs/4T.jpg';
-var img2 = '/img/thumbs/6T.jpg';
+var img0 = '/img/fullsize/1.jpg';
+var img1 = '/img/fullsize/2.jpg';
+var img2 = '/img/fullsize/3.jpg';
 
 // Show Images
 $('header img').attr('src', eval('img' + numRand) );
