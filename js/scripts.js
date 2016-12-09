f5891b10f7166fa0d25f61c7ace1d681253235bb
kschneider726 <kschneider@Kelbes-MacBook-Pro.local>
2016-12-08 20:28:49 -0800

    last changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var randNum = Math.floor(Math.random() * max );
 console.log(randNum);
 
 // Store Images
-var img0 = '/img/thumbs/Tbuda1.jpg';
-var img1 = '/img/thumbs/Tbuda2.jpg';
-var img2 = '/img/thumbs/Tmexico1.jpg';
+var img0 = 'img/thumbs/Tbuda1.jpg';
+var img1 = 'img/thumbs/Tbuda2.jpg';
+var img2 = 'img/thumbs/Tmexico1.jpg';
 
 
 // Show Images
